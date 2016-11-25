3eb455b9db2446a5c9f5f47de81dd95afb494652
domenico0892 <giammarinodomenico@gmail.com>
2016-11-25 14:13:47 +0100

    buld signal component svo multithread

--- src/org/rm3umf/framework/buildSVOmodel/BuiltSVOSignalComponent.java
+++ src/org/rm3umf/framework/buildSVOmodel/BuiltSVOSignalComponent.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.*;
 import org.rm3umf.domain.Concept;
@@ -47,13 +50,25 @@ public class BuiltSVOSignalComponent {
 	public void createSignalComponent(List<Period> listaPeriodi) throws PersistenceException, ExtractorException{/*
 	 *Creo i signal componente relativi relative ai periodi 
 	 */
+		ExecutorService es = Executors.newCachedThreadPool();
 		for(Period period :listaPeriodi.subList(0, listaPeriodi.size())){
 			System.out.println("Costruisco i SignalComponent per il periodo "+period);
 			//salvo il periodo
 			AAFacadePersistence.getInstance().periodSave(period);
 			//il periodo crea tutte le signal component prensenti in esso
-			extractSignalComponent(period);
-
+			try {
+				es.submit(new ExtractSignalComponentByPeriod(strategyExtractor, alfa, beta, gamma, period));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		try {
+			es.awaitTermination(10, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -65,95 +80,4 @@ public class BuiltSVOSignalComponent {
 	 * @throws ExtractorException
 	 * @throws PersistenceException
 	 */
-	public void extractSignalComponent(Period period) throws ExtractorException, PersistenceException{
-		logger.info("creo i signal component");
-		List<PseudoFragment> listaPseudo=AAFacadePersistence.getInstance().pseudoDocumentGetByPeriod(period);
-		for (PseudoFragment pf : listaPseudo)
-		extractSignalComponentByPseudoFragment(pf);
-	}
-
-	public void extractSignalComponentByPseudoFragment (PseudoFragment pf) throws ExtractorException, PersistenceException {
-
-		Map<Concept, List<Message>> concept2Messages = concept2Messages(pf);
-		Map<Message, Integer> message2sentiment = message2sentiment(pf);
-		
-		SignalComponent sigComp;
-		
-		double s, v, o, svo;
-		int p, neg, neu;
-
-		for (Concept c : concept2Messages.keySet()) {
-			v = (double) concept2Messages.get(c).size() / pf.getMessages().size();;
-			p = getPositiveMessages(message2sentiment);
-			neg = getNegativeMessages(message2sentiment);
-			neu = getNeutralMessages(message2sentiment);
-			o = neu / (neu + neg + p);
-			s = 1 / (1 + Math.pow(10, -((p - neg)/(p + neg))));
-			svo = alfa * s + beta * v + gamma * o;
-			sigComp = new SignalComponent();
-			sigComp.setConcept(c);
-			sigComp.setPeriod(pf.getPeriod());
-			sigComp.setUser(pf.getUser());
-			sigComp.setTfidf(svo);
-			AAFacadePersistence.getInstance().signalComponentSave(sigComp);
-		}
-
-
-	}
-
-	private Map<Concept, List<Message>> concept2Messages (PseudoFragment pf) throws ExtractorException {
-		List<Concept> concepts;
-		List<Message> l;
-		Map<Concept, List<Message>> concept2Messages = new HashMap<>();
-		for (Message m : pf.getMessages()) {
-			concepts = this.strategyExtractor.extract(m);
-			for (Concept c : concepts) {
-				if (!concept2Messages.containsKey(c)) {
-					l = new LinkedList<Message>();
-					l.add(m);
-					concept2Messages.put(c, l);
-				}
-				else {
-					l = concept2Messages.get(c);
-					l.add(m);
-				}
-			}
-		}
-		return concept2Messages;
-	}
-
-	private Map<Message, Integer> message2sentiment (PseudoFragment pf) {
-		Map<Message, Integer> message2sentiment = new HashMap<>();
-		for (Message m : pf.getMessages())
-			message2sentiment.put(m, this.sentimentResolver.getSentiment(m.getText()));
-		return message2sentiment;
-	}
-	
-	private int getPositiveMessages (Map<Message, Integer> map) {
-		int n = 0;
-		for (Message m : map.keySet() ) {
-			if (map.get(m) > 0)
-				n++;
-		}
-		return n;
-	}
-	
-	private int getNeutralMessages (Map<Message, Integer> map) {
-		int n = 0;
-		for (Message m : map.keySet() ) {
-			if (map.get(m) == 0)
-				n++;
-		}
-		return n;
-	}
-	
-	private int getNegativeMessages (Map<Message, Integer> map) {
-		int n = 0;
-		for (Message m : map.keySet() ) {
-			if (map.get(m) < 0)
-				n++;
-		}
-		return n;
-	}
-
 }
---
+++ src/org/rm3umf/framework/buildSVOmodel/ExtractSignalComponentByPeriod.java
@@ -0,0 +1,154 @@
+package org.rm3umf.framework.buildSVOmodel;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.rm3umf.domain.Concept;
+import org.rm3umf.domain.Message;
+import org.rm3umf.domain.Period;
+import org.rm3umf.domain.PseudoFragment;
+import org.rm3umf.domain.SignalComponent;
+import org.rm3umf.framework.buildmodel.extractor.ExtractorException;
+import org.rm3umf.framework.buildmodel.extractor.StrategyExtraction;
+import org.rm3umf.persistenza.AAFacadePersistence;
+import org.rm3umf.persistenza.PersistenceException;
+import org.rm3umf.sentiment.SentimentResolver;
+
+public class ExtractSignalComponentByPeriod implements Runnable {
+
+	private StrategyExtraction strategyExtractor;
+	private SentimentResolver sentimentResolver;
+	private double alfa;
+	private double beta;
+	private double gamma;
+	private Period period;
+
+
+	public ExtractSignalComponentByPeriod (StrategyExtraction strategyExtractor, double a, double b, double c, Period p) throws Exception {
+		this.strategyExtractor=strategyExtractor;
+		this.sentimentResolver = new SentimentResolver("en");
+		this.alfa = a;
+		this.beta = b;
+		this.gamma = c;
+		this.period = p;
+	}
+
+	public void run () {
+
+		
+		List<PseudoFragment> listaPseudo;
+		try {
+			listaPseudo = AAFacadePersistence.getInstance().pseudoDocumentGetByPeriod(period);
+			for (PseudoFragment pf : listaPseudo) {
+				extractSignalComponentByPseudoFragment(pf);
+			}
+		} catch (PersistenceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+	public void extractSignalComponentByPseudoFragment (PseudoFragment pf) {
+
+		Map<Concept, List<Message>> concept2Messages = null;
+		try {
+			concept2Messages = concept2Messages(pf);
+		} catch (ExtractorException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Map<Message, Integer> message2sentiment = message2sentiment(pf);
+
+		SignalComponent sigComp;
+
+		double s, v, o, svo;
+		int p, neg, neu;
+
+		for (Concept c : concept2Messages.keySet()) {
+			v = (double) concept2Messages.get(c).size() / pf.getMessages().size();;
+			p = getPositiveMessages(message2sentiment);
+			neg = getNegativeMessages(message2sentiment);
+			neu = getNeutralMessages(message2sentiment);
+			o = neu / (neu + neg + p);
+			s = 1 / (1 + Math.pow(10, -((p - neg)/(p + neg))));
+			svo = alfa * s + beta * v + gamma * o;
+			sigComp = new SignalComponent();
+			sigComp.setConcept(c);
+			sigComp.setPeriod(pf.getPeriod());
+			sigComp.setUser(pf.getUser());
+			sigComp.setTfidf(svo);
+			try {
+				AAFacadePersistence.getInstance().signalComponentSave(sigComp);
+			} catch (PersistenceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+
+	}
+
+	private Map<Concept, List<Message>> concept2Messages (PseudoFragment pf) throws ExtractorException {
+		List<Concept> concepts;
+		List<Message> l;
+		Map<Concept, List<Message>> concept2Messages = new HashMap<>();
+		for (Message m : pf.getMessages()) {
+			concepts = this.strategyExtractor.extract(m);
+			for (Concept c : concepts) {
+				if (!concept2Messages.containsKey(c)) {
+					l = new LinkedList<Message>();
+					l.add(m);
+					concept2Messages.put(c, l);
+				}
+				else {
+					l = concept2Messages.get(c);
+					l.add(m);
+				}
+			}
+		}
+		return concept2Messages;
+	}
+
+	private Map<Message, Integer> message2sentiment (PseudoFragment pf) {
+		Map<Message, Integer> message2sentiment = new HashMap<>();
+		for (Message m : pf.getMessages())
+			message2sentiment.put(m, this.sentimentResolver.getSentiment(m.getText()));
+		return message2sentiment;
+	}
+
+	private int getPositiveMessages (Map<Message, Integer> map) {
+		int n = 0;
+		for (Message m : map.keySet() ) {
+			if (map.get(m) > 0)
+				n++;
+		}
+		return n;
+	}
+
+	private int getNeutralMessages (Map<Message, Integer> map) {
+		int n = 0;
+		for (Message m : map.keySet() ) {
+			if (map.get(m) == 0)
+				n++;
+		}
+		return n;
+	}
+
+	private int getNegativeMessages (Map<Message, Integer> map) {
+		int n = 0;
+		for (Message m : map.keySet() ) {
+			if (map.get(m) < 0)
+				n++;
+		}
+		return n;
+	}
+
+}
+
+
+
+
